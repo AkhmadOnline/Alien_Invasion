93bd733c6a165dad1427400ec38d88f1153a5e12
Akhmad <theionline2019@gmail.com>
2024-09-25 22:37:44 +0300

    Добавление пришельцев

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры"""
@@ -18,6 +19,8 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
 
     def run_game(self):
         """Запуск основного цикла игры"""
@@ -59,6 +62,12 @@ class AlienInvasion:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
 
+    def _create_fleet(self):
+        """Создание флота вторжения"""
+        #Создание пришельца
+        alien = Alien(self)
+        self.aliens.add(alien)
+
     def _update_bullets(self):
         #Обновление позиции снарядов
         self.bullets.update()
@@ -73,6 +82,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         pygame.display.flip()
 
 if __name__ == '__main__':
